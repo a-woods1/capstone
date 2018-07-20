c83e8dffbb449cf832c99275cd8fe3fa169bf6cd
Aaron Faucher <aaron.faucher@gmail.com>
2018-07-19 21:28:10 -0400

    Styled the My Requests page

--- prototype/static/src/components/Main/Requests/Requests.js
+++ prototype/static/src/components/Main/Requests/Requests.js
@@ -5,7 +5,7 @@ import * as actionCreators from '../../../actions/auth';
 import { Grid, Row, Col, Glyphicon,ButtonToolbar, Button } from 'react-bootstrap';
 
 import RequestList from './RequestList/RequestList.js';
-
+import contact from '../../../images/contact.png';
 
 function mapStateToProps(state) {
     return {
@@ -28,43 +28,46 @@ class Requests extends React.Component {
     const hasRequests = requests != null
     console.log(hasRequests)
     return (
-     <div>
+     <div id="requests">
       <Grid>
-      <Row>
-        <Col xs={12} md={12}>
-          <div>My Requests > Current Requests</div>
-        </Col>
-      </Row>
         <Row>
           <Col xs={12} md={12}>
             <h1>Current Requests</h1>
           </Col>
         </Row>
-        {hasRequests
-        ?
+        <div className="request-table">
+          {hasRequests
+          ?
+          <Row className="request-table-header">
+            <Col xs={1} md={1}></Col>
+            <Col xs={3} md={3}>
+              <div>Accommodation</div>
+            </Col>
+            <Col xs={2} md={2}>
+              <div>Interview Date</div>
+            </Col>
+            <Col xs={2} md={2}>
+              <div>Interview Stage</div>
+            </Col>
+            <Col xs={2} md={2}>
+              <div>Location</div>
+            </Col>
+            <Col xs={2} md={2}>
+              <div>Status</div>
+            </Col>
+          </Row>
+          :
+          <div></div>
+          }
+          <RequestList />
+        </div>
         <Row>
-          <Col xs={1} md={1}></Col>
-          <Col xs={2} md={2}>
-            <div>Accommodation</div>
-          </Col>
-          <Col xs={2} md={2}>
-            <div>Interview Date</div>
-          </Col>
-          <Col xs={2} md={2}>
-            <div>Interview Stage</div>
-          </Col>
-          <Col xs={2} md={2}>
-            <div>Location</div>
-          </Col>
-          <Col xs={2} md={2}>
-            <div>Status</div>
-          </Col>
-          <Col xs={1} md={1}></Col>
-        </Row>
-      :
-      <div></div>
-    }
-    <RequestList />
+          <Col xs={12} md={12}>
+            <div className="contact-recruiter">
+              <a href="#"><img src={contact} />Contact Recruiter</a>
+            </div>
+          </Col>    
+        </Row>    
     </Grid>
     </div>
     );
