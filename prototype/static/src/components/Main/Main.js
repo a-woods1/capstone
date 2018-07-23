6f888afbc936f20500c214f2085b82be358b1561
Aaron Faucher <aaron.faucher@gmail.com>
2018-07-22 20:37:53 -0400

    Revert "Merge branch 'dev-styles' into master"
    This reverts commit 1fa2418a988cbd25fa1e3377b4121cae7de881a0, reversing
    changes made to 75f4dfd10fd750dababdb272471937ddd36229dd.

--- prototype/static/src/components/Main/Main.js
+++ prototype/static/src/components/Main/Main.js
@@ -143,7 +143,7 @@ class Main extends Component { // eslint-disable-line react/prefer-stateless-fun
 
   render() {
     return (
-      <div id="text-content" className="col-xs-8">
+      <div>
         <Grid>
           <Overview />
           <Arrival />
