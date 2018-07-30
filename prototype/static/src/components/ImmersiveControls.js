09b5f6dbe0ce5d679ffb35ce496c3a53fbe99151
Aaron Faucher <aaron.faucher@gmail.com>
2018-07-30 05:51:35 -0400

    Escape to close immersive

--- prototype/static/src/components/ImmersiveControls.js
+++ prototype/static/src/components/ImmersiveControls.js
@@ -7,11 +7,25 @@ class ImmersiveControls extends Component {
 
   constructor(props) {
     super(props);
+
+    this.closeOnEsc = this.closeOnEsc.bind(this);
+
   }
 
   componentDidMount() {
+     window.addEventListener('keydown', this.closeOnEsc);
+   }
+   
+   componentWillUnmount() {
+     window.removeEventListener('scroll', this.closeOnEsc);
+   }
 
-  }
+  closeOnEsc(e) {
+     // esc key
+    if(e.keyCode == 27) {
+      this.props.close();
+    }
+   }
 
   render () {
       return (
